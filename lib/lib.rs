06429de3aebde1ae1a173d478ada37af4b097283
John Zacarias Jekel <jzjisawesome123@hostmail.com>
2023-07-16 10:27:45 +0000

    More trait progress

--- lib/lib.rs
+++ lib/lib.rs
@@ -59,37 +59,51 @@ use std::ops::Range;
  * Traits And Default Implementations
  * --------------------------------------------------------------------------------------------- */
 
-//TODO
-
-/* ------------------------------------------------------------------------------------------------
- * Trait Implementations
- * --------------------------------------------------------------------------------------------- */
-
 trait BitManip: Sized {
-    /*const*/ fn bit(&self, bit: Self) -> Self;
-    /*const*/ fn bits(&self, range: Range<Self>) -> Self;
+    type BitIndex;
 
-    /*const*/ fn set_bit(&self, bit: Self) -> Self;
-    /*const*/ fn set_bit_assign(&mut self, bit: Self);
-    /*const*/ fn set_bits(&self, range: Range<Self>) -> Self;
-    /*const*/ fn set_bits_assign(&mut self, range: Range<Self>);
+    //TODO make all of these const too since we only intend for this to be implemented for integers
 
-    /*const*/ fn clear_bit(&self, bit: Self) -> Self;
-    /*const*/ fn clear_bit_assign(&mut self, bit: Self);
-    /*const*/ fn clear_bits(&self, range: Range<Self>) -> Self;
-    /*const*/ fn clear_bits_assign(&mut self, range: Range<Self>);
+    fn bit(&self, bit_index: Self::BitIndex) -> Self;
+    fn bits(&self, range: Range<Self::BitIndex>) -> Self;
 
-    /*const*/ fn toggle_bit(&self, bit: Self) -> Self;
-    /*const*/ fn toggle_bit_assign(&mut self, bit: Self);
-    /*const*/ fn toggle_bits(&self, range: Range<Self>) -> Self;
-    /*const*/ fn toggle_bits_assign(&mut self, range: Range<Self>);
+    fn set_bit(&self, bit_index: Self::BitIndex) -> Self;
+    fn set_bits(&self, range: Range<Self::BitIndex>) -> Self;
+    fn set_bit_assign(&mut self, bit_index: Self::BitIndex) {
+        *self = self.set_bit(bit_index);
+    }
+    fn set_bits_assign(&mut self, range: Range<Self::BitIndex>) {
+        *self = self.set_bits(range);
+    }
 
-    /*const*/ fn replace_bit(&self, bit: Self, value: Self) -> Self;
-    /*const*/ fn replace_bit_assign(&mut self, bit: Self value: Self);
-    /*const*/ fn replace_bits(&self, range: Range<Self>, value: Self) -> Self;
-    /*const*/ fn replace_bits_assign(&mut self, range: Range<Self>, value: Self);
+    fn clear_bit(&self, bit_index: Self::BitIndex) -> Self;
+    fn clear_bits(&self, range: Range<Self::BitIndex>) -> Self;
+    fn clear_bit_assign(&mut self, bit_index: Self::BitIndex) {
+        *self = self.clear_bit(bit_index);
+    }
+    fn clear_bits_assign(&mut self, range: Range<Self::BitIndex>) {
+        *self = self.clear_bits(range);
+    }
 
-    /*const*/ fn bitmask(range: Range<Self>) -> Self;
+    fn toggle_bit(&self, bit_index: Self::BitIndex) -> Self;
+    fn toggle_bits(&self, range: Range<Self::BitIndex>) -> Self;
+    fn toggle_bit_assign(&mut self, bit_index: Self::BitIndex) {
+        *self = self.toggle_bit(bit_index);
+    }
+    fn toggle_bits_assign(&mut self, range: Range<Self::BitIndex>) {
+        *self = self.toggle_bits(range);
+    }
+
+    fn replace_bit(&self, bit_index: Self::BitIndex, value: impl Into<bool>) -> Self;
+    fn replace_bits(&self, range: Range<Self::BitIndex>, value: Self) -> Self;
+    fn replace_bit_assign(&mut self, bit_index: Self::BitIndex, value: impl Into<bool>) {
+        *self = self.replace_bit(bit_index, value);
+    }
+    fn replace_bits_assign(&mut self, range: Range<Self::BitIndex>, value: Self) {
+        *self = self.replace_bits(range, value);
+    }
+
+    fn bitmask(range: Range<Self::BitIndex>) -> Self;
 
     //TODO others
 }
@@ -100,6 +114,12 @@ trait BitManip: Sized {
     }
     //TODO others
 }*/
+
+/* ------------------------------------------------------------------------------------------------
+ * Trait Implementations
+ * --------------------------------------------------------------------------------------------- */
+
+//TODO
 
 /* ------------------------------------------------------------------------------------------------
  * Functions
